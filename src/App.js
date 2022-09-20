dc148d4dfe6b48917cf50eec3cb19e2d6f84ef44
selly7183 <selly7183@naver.com>
2022-09-20 21:26:28 +0900

    리스트 추가, 삭제 기능 추가  commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { useState, useRef } from "react";
 import "./App.scss";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
 function App() {
+	const [data, setData] = useState([]);
+
+	const dataId = useRef(0);
+
+	const onCreate = (author, content, emotion) => {
+		const created_date = new Date().getTime();
+		const newItem = {
+			onDelete,
+			author,
+			content,
+			emotion,
+			created_date,
+			id: dataId.current,
+		};
+		dataId.current += 1;
+		setData([newItem, ...data]);
+	};
+
+	const onDelete = (targetId) => {
+		console.log(`${targetId}가 삭제되었습니다.`);
+		const newDiaryList = data.filter((it) => it.id !== targetId);
+		setData(newDiaryList);
+	};
+
 	return (
 		<div className="App">
-			<DiaryEditor />
+			<DiaryEditor onCreate={onCreate} />
+			<DiaryList onDelete={onDelete} diaryList={data} />
 		</div>
 	);
 }
