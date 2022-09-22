537e265d3a59f321e1577de7ef4f8519a50f0b10
selly7183 <selly7183@naver.com>
2022-09-22 17:23:04 +0900

    useMemo 사용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useMemo } from "react";
 import "./App.scss";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 import LifeCycle from "./LifeCycle";
-
-// https://jsonplaceholder.typicode.com/comments
 
 function App() {
 	const [data, setData] = useState([]);
@@ -62,10 +60,25 @@ function App() {
 		);
 	};
 
+	const getDiaryAnalysis = useMemo(() => {
+		console.log("일기 분석 시작");
+
+		const goodCount = data.filter((it) => it.emotion >= 3).length;
+		const badCount = data.length - goodCount;
+		const goodRatio = (goodCount / data.length) * 100;
+		return { goodCount, badCount, goodRatio };
+	}, [data.length]);
+
+	const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
 	return (
 		<div className="App">
 			<LifeCycle />
 			<DiaryEditor onCreate={onCreate} />
+			<div>전체 일기 : {data.length}</div>
+			<div>기분 좋은 일기 개수 : {goodCount}</div>
+			<div>기분 나쁜 일기 개수 : {badCount}</div>
+			<div>기분 좋은 일기 비율 : {goodRatio}</div>
 			<DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
 		</div>
 	);
